009c84c76c6c95f11a33b9a80f87b8add4ea790a
EmileSabastian <32434605+EmileSabastian@users.noreply.github.com>
2022-07-06 16:57:03 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'dbrds1.cxll0zaobz3h.ap-southeast-1.rds.amazonaws.com',
+    host: 'database-2.cqfsejwoeaxk.us-east-1.rds.amazonaws.com',
     port: '3306',
     user: 'admin',
     password: 'password',
