38682f5a39a27d8956d363352ae4ecd54b907ebc
EmileSabastian <emile.sabastian@gmail.com>
2022-07-06 15:45:21 +0800

    first

---
+++ db-connection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'dbrds1.cxll0zaobz3h.ap-southeast-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'password',
+    database: 'movie_info'
+});
+
+connection.connect(err => {  // test out connetion and console.log error if there is one
+    if (err) throw err;
+    console.log('Connected To AWS DB');
+}); 
+module.exports = connection;
+
